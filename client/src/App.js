364d73e956c9cd3d4505fa9618458b484abe368e
oshan-sameera <oshan951@gmail.com>
2021-08-19 11:48:45 +1000

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Oshan App</h1>
+          <h1>Oshan Sameera App</h1>
         </p>
         <a
           className="App-link"
